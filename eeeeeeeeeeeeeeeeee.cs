770f85baa8165405ef13d7f694fea135ffcaecba
wonderhowlongitwillletmetypethismessage <alk433@stemcollegiate.ca>
2025-02-19 08:32:31 -0800

    Update eeeeeeeeeeeeeeeeee.cs

--- eeeeeeeeeeeeeeeeee.cs
+++ eeeeeeeeeeeeeeeeee.cs
@@ -3,3 +3,11 @@ int zorg=999;
 Console.WriteLine(zorg);
 //random comment//
 Console.WriteLine("eee");
+
+//feb 19//
+string firstname;
+firstname = "gort";
+Console.WriteLine(firstname);
+string lastname;
+lastname = " zort";
+Console.WriteLine(lastname);
